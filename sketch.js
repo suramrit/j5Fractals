e966adaa1580819e06ab20dd7ac25abd884673ad
suramrit <suramrit@buffalo.edu>
2017-02-02 05:38:55 -0500

    fractals3

--- sketch.js
+++ sketch.js
@@ -13,36 +13,86 @@ b --   |
 	   |
 needs treminating condition
 */
+
+
+//branch obj.. start end display function.. 
+
 var angle = 0;
 var slider;
+var tree = [];
+var count = 0;
+var leaves = [];
 
 function setup(){
 	createCanvas(800,800); //bckgrnd
 	slider = createSlider(0,TWO_PI,PI/2,0.01);
+	var a = createVector(width/2, height);
+	var b = createVector(width/2,height-100);
+	var rut = new branch(a,b);
+	tree[0] = rut;
+
+}
+
+function mousePressed(){
+
+	for(var i=tree.length-1; i>=0; i--){
+		if(!tree[i].finished){
+		tree.push(tree[i].brnchA());
+		tree.push(tree[i].brnchB());}
+		tree[i].finished=true;
+	};
+	//tree[1] = tree[0].brnchA();
+	//tree[2] = tree[0].brnchB();
+	count++;
+	if(count===6){
+		for(var i=0; i<tree.length; i++){
+				if(!tree[i].finished){
+					var leaf = (tree[i].end.copy());
+					leaves.push(leaf);
+				}
+		}
+	}
 }
 
 function draw(){
 	background(50);
-	angle = slider.value();
-	stroke(225);
+	for(var i=0;i<tree.length;i++)
+	{
+		tree[i].show();
+		//tree[i].jitter();
+	}
+
+	for(var i=0;i<leaves.length;i++)
+	{
+		fill(255,0,100,1000);
+		noStroke();
+		ellipse(leaves[i].x, leaves[i].y, 8,8);
+		//leaves[i].y += random(0,1);
+
+		//tree[i].jitter();
+	}
+	//angle = slider.value();
+
+	/*stroke(230);
 	translate(400,height);
-	branch(250);
-	 //trunk.. 
+	branch(250);*/
+	 //trunk..
+	  
 }
 
-function branch(len){
+/*function branch(len){
 	
 	line(0,0, 0, -len);
 	translate(0,-len);
 	if(len>2){
 	push();
 	rotate(angle);
-	branch(len*0.72);
+	branch(len*0.68);
 	pop();
 	push();
 	rotate(-angle);
-	branch(len*0.60);
+	branch(len*0.68);
 	pop();
 	}
 	//line(0,0,0,-len * 0.67);
-}
+}*/
